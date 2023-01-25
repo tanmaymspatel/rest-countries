a0294fb4bbc487ceaf8e4fbdfb705591eea0b84b
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-25 20:00:06 +0530

    back button

--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -1,18 +1,38 @@
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import restservices from '../services/restServices'
 
 function CountryDetails() {
 
-    const pr = useParams()
+    const { name } = useParams();
+    const { getSingleCountryDetails } = restservices;
+    const navigate = useNavigate();
 
     useEffect(() => {
-        console.log(pr);
-    }, [pr]);
+        getSingleCountryDetails(name as string).then(res => console.log(res.data[0]))
+    }, [getSingleCountryDetails, name]);
 
-    return (
-        <div>
-            country details
-        </div>
+    return (<>
+        <section className='section'>
+            <div className='container'>
+                <div className='btn-bacak'>
+                    <button className='btn btn-light' onClick={() => navigate(-1)}>
+                        <span>{`<-`}</span>
+                        <span>Back</span>
+                    </button>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className='row justify-content-between align-items-center'>
+                <div className='details-box'>
+                    <figure></figure>
+                </div>
+                <div className='details-box'></div>
+            </div>
+        </section>
+    </>
     )
 };
 
