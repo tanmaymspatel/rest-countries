104b1cd6837bcab5fdc32f3c81b7d80f68b44295
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-27 16:09:09 +0530

    loader added

---
+++ src/shared/Loader.tsx
@@ -0,0 +1,10 @@
+function Loader() {
+    return (
+        <div className="h-100 d-flex align-items-center justify-content-center">
+            <div className="pac-man">
+            </div>
+        </div>
+    )
+};
+
+export default Loader;
