591b0ebd8db0e110839f0d581b1136bd39b3bef4
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-27 15:18:31 +0530

    back button icon

--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -42,8 +42,8 @@ function CountryDetails() {
             <section className='section'>
                 <div className='container'>
                     <div className='btn-bacak'>
-                        <button className='btn btn-light' onClick={() => navigate(-1)}>
-                            <span>{`<-`}</span>
+                        <button className='btn btn-light d-flex align-items-center justify-content-center' onClick={() => navigate(-1)}>
+                            <span className='icon-back'></span>
                             <span>Back</span>
                         </button>
                     </div>
